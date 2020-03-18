f1b4dcf7011708656cc5dae85cd961348e487710
Julian Blagis <julianbgs96@gmail.com>
2020-03-18 23:22:15 +0200

    create animation

---
+++ src/components/ScrollBg/Infinite.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Infinite.sass';
+
+class Infinite extends React.Component{
+  render() {
+    return(
+      <div className="Infinite">
+      </div>
+    )
+  }
+}
+
+export default Infinite;
--- src/components/Wrapper/Wrap.jsx
+++ src/components/Wrapper/Wrap.jsx
@@ -10,6 +10,7 @@ import sym4 from "../../assets/Images/SYM4.png";
 import sym5 from "../../assets/Images/SYM5.png";
 import sym6 from "../../assets/Images/SYM6.png";
 import sym7 from "../../assets/Images/SYM7.png";
+import Infinite from "../ScrollBg/Infinite";
 
 class Wrapper extends React.Component {
 
@@ -27,9 +28,11 @@ class Wrapper extends React.Component {
   }
 
   refreshImage = () => {
+    this.animationProcessStart();
     let idx1 = this.getInterval(0, 5);
     let idx2 = this.getInterval(0, 5);
     let idx3 = this.getInterval(0, 5);
+
     this.setState((state) => ({
       index: [idx1, idx2, idx3],
       count: state.count + 1
@@ -38,6 +41,7 @@ class Wrapper extends React.Component {
     if(this.state.count === 4) {
       this.winCombination();
     }
+    this.animationProcessEnd();
   };
 
   winCombination = () => {
@@ -45,8 +49,41 @@ class Wrapper extends React.Component {
     win1 = win2 = win3 = this.getInterval(0, 5);
     this.setState((state) => ({
       index: [win1, win2, win3],
-      count: 0
+      count: 0,
+      scrollElem: null
     }));
+  };
+
+  animationProcessStart = () => {
+
+    let startAnimate = new Promise((resolve, reject) => {
+      resolve();
+    });
+    startAnimate.then(() => {
+      let elem = [];
+      elem.push(
+        <div className="Box" key={this.getInterval(0, 10)}>
+          <Infinite />
+        </div>
+      )
+
+      this.setState((state) => ({
+          scrollElem: elem
+        }));
+    });
+  };
+  animationProcessEnd = () => {
+    let endAnimate = new Promise((resolve, reject) => {
+     setTimeout(() => {
+       resolve();
+     },3000)
+   });
+
+   endAnimate.then(() => {
+     this.setState((state) => ({
+       scrollElem: state.scrollElem.splice(2, 0)
+     }));
+   });
   };
 
   getInterval = (min, max) => {
@@ -57,9 +94,11 @@ class Wrapper extends React.Component {
 
   getComponent = ()  => {
     let elements = [];
+    let scrollElem = this.state.scrollElem;
     for(let i = 0; i < 3; i++) {
       elements.push(<div className="Wrapper__Elem" key={i}>
         <Elem getImage={this.state.array[this.state.index[i]]} getInterval={this.getInterval} />
+        {scrollElem}
       </div>)
     }
     return elements;
@@ -68,7 +107,7 @@ class Wrapper extends React.Component {
   render() {
     return(
       <div className="Wrapper">
-        { this.getComponent() }
+        { this.getComponent()}
         <Button refreshImage={this.refreshImage} />
       </div>
     )
